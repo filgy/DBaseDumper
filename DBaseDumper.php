f1c30d48c27a185e1ee28a4e50df144b321212a7
Filgy <filgy@sniff.cz>
2011-08-26 20:12:09 +0200

    Implemented selecting driver in DBaseDriver

--- DBaseDumper.php
+++ DBaseDumper.php
@@ -6,19 +6,24 @@
 	* @author			Filgy (filgy@sniff.cz)
 	* @package			DBaseDumper (Database dumper)
 	* @license			GNU/GPL v2
-	* @update			26.8.2011 19:45
+	* @update			26.8.2011 20:10
 	*/
 
 	class DBaseDumper{
 		
-		private $driver = NULL;
+		private $DBaseDriver = NULL;
 		
 		public function __construct(Array $config){
+			$driver = "DBaseDriver".ucfirst($config['driver']);
 			
+			if(!class_exists($driver))
+				throw new DBaseException("Undefined driver");
+			
+			$this->DBaseDriver = new $driver($config);
 		}
 		
 		public function __call($name, $args){
-			
+
 		}
 	
 	};
