13e9f728e7f2ee6574fe2bc1e9057ef6782163c3
Filgy <filgy@sniff.cz>
2011-08-26 19:13:25 +0200

    Preimplemented final class DBaseDriverMySQL

--- DBaseDrivers.php
+++ DBaseDrivers.php
@@ -6,7 +6,7 @@
 	* @author			Filgy (filgy@sniff.cz)
 	* @package			DBaseDumper (Database dumper)
 	* @license			GNU/GPL v2
-	* @update			26.8.2011 19:10
+	* @update			26.8.2011 19:12
 	*/
 	
 	abstract class DBaseDriver{
@@ -15,5 +15,32 @@
 		
 		public function __construct(Array $config){
 			$this->config = $config;
+		}
+	};
+	
+	final class DBaseDriverMySQL extends DBaseDriver implements DBaseDriverI{
+		
+		public function __construct(Array $config){
+			parent::__construct($config);
+		}
+		
+		public function singleRow($sql){
+			
+		}
+		
+		public function singleColumn($sql){
+			
+		}
+		
+		public function query($sql){
+			
+		}
+		
+		public function escape($string){
+			
+		}
+		
+		private function getConnection(){
+			
 		}
 	};
