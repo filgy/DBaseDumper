e23cd36c36af538306db31d1828b9e4a957d3c36
Filgy <filgy@sniff.cz>
2011-08-30 17:49:29 +0200

    Created interface DBaseModelI

--- DBaseInterfaces.php
+++ DBaseInterfaces.php
@@ -6,7 +6,7 @@
 	* @author			Filgy (filgy@sniff.cz)
 	* @package			DBaseDumper (Database dumper)
 	* @license			GNU/GPL v2
-	* @update			26.8.2011 21:45
+	* @update			30.8.2011 17:47
 	*/
 
 	interface DBaseDriverI{
@@ -20,6 +20,10 @@
 		public function clearResult();
 		
 		public function escape($string);	
+	};
+	
+	interface DBaseModelI{
+		public function __construct(Array $config);
 		
 		public function showTables($dbName);
 		public function showColumns($dbName, $tableName);
